59e4e96fb8de797a7d55dbe5a55f56f3c97d26fe
VoidGeek <pradyumna.p.6969@gmail.com>
2023-11-04 21:58:07 +0530

    Update

--- src/services/testimonial.service.js
+++ src/services/testimonial.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = process.env.NODE_ENV === "production" ? "https://vercel-api-ashen-iota.vercel.app/api" : "/api";  // Replace with your actual API URL
+const API_URL = process.env.NODE_ENV === "production" ? "/api" : "/api";  // Replace with your actual API URL
 
 // Fetch all testimonials
 const getAllTestimonials = () => {
